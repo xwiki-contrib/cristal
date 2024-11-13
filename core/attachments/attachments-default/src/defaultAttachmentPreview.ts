40fc746d8b6ea68c5908b6bf6ec796ce04c77106
Manuel Leduc <manuel.leduc@xwiki.com>
2024-11-13 15:18:33 +0100

    --wip-- [skip ci]

--- core/attachments/attachments-default/src/defaultAttachmentPreview.ts
+++ core/attachments/attachments-default/src/defaultAttachmentPreview.ts
@@ -1,6 +1,8 @@
 import { AttachmentPreview } from "@xwiki/cristal-attachments-api";
 import { AttachmentReference } from "@xwiki/cristal-model-api";
+import { injectable } from "inversify";
 
+@injectable()
 class DefaultAttachmentPreview implements AttachmentPreview {
   preview(attachment: AttachmentReference): void {
     console.log("attachment", attachment);
