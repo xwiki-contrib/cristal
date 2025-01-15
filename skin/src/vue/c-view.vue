689ffbc9601d5cd06b9073b0612f5b3c3ea36bb5
Thiago Krieck <thiago.krieck@xwiki.com>
2025-01-15 11:02:04 -0300

    CRISTAL-422: Implement styles for external links
    * Implemented an icon for the external links as it is in XWiki Standard

--- skin/src/vue/c-view.vue
+++ skin/src/vue/c-view.vue
@@ -323,6 +323,25 @@ TODO: Discuss and move them to a more appropriate place
 */
 :deep(.wikiexternallink) {
   font-style: italic;
+  position: relative;
+  display: inline-flex;
+}
+
+:deep(.wikiexternallink a:after) {
+  /*
+  TODO: Try to make this customisable, it is dependant on bootstrap icons for now.
+  */
+  content: "\F1C5";
+  display: inline-block;
+  font-family: bootstrap-icons;
+  font-style: normal;
+  font-weight: bold;
+  text-transform: none;
+  line-height: 1;
+  -webkit-font-smoothing: antialiased;
+  font-size: 0.7rem;
+  text-decoration: none;
+  margin-left: 4px;
 }
 
 @container xwCristal (max-width: 600px) {
