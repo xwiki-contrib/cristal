876dd7cc5a26b32f66308033ed03f07f5e665354
Clément E. <clement.eraud@xwiki.com>
2025-03-20 12:01:40 +0100

    --wip-- [skip ci]

--- editors/blocknote/src/utils.ts
+++ editors/blocknote/src/utils.ts
@@ -1,3 +1,23 @@
+/*
+ * See the LICENSE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 function debounce<T>(func: (arg: T) => unknown): (arg: T) => void {
   let running = false;
 
@@ -48,7 +68,7 @@ function stringToColor(str: string, prc?: number) {
     (G < 255 ? (G < 1 ? 0 : G) : 255) * 0x100 +
     (B < 255 ? (B < 1 ? 0 : B) : 255);
 
-  return comp.toString(16).slice(1);
+  return `#${comp.toString(16).slice(1)}`;
 }
 
 export { debounce, stringToColor };
