d27ae1de5fe82cf5f84bd02f161e203cf8c01c86
Clément E. <clement.eraud@xwiki.com>
2025-04-25 11:11:26 +0200

    Add toolbar for images

--- editors/blocknote-headless/src/vue/blocks/ImageToolbar.vue
+++ editors/blocknote-headless/src/vue/blocks/ImageToolbar.vue
@@ -21,6 +21,7 @@ Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 import ImageFilePanel from "./ImageFilePanel.vue";
 import { BlockOfType, EditorType } from "../../blocknote";
 import { LinkEditionContext } from "../../components/linkEditionContext";
+import { CIcon } from "@xwiki/cristal-icons";
 import { ref, watch } from "vue";
 
 const {
@@ -38,13 +39,70 @@ const url = ref(image.props.url);
 watch(url, (url) => {
   editor.updateBlock({ id: image.id }, { props: { url } });
 });
+
+const showImagePanel = ref(false);
 </script>
 
 <template>
-  <ImageFilePanel
-    :current-block="image"
-    :link-edition-ctx
-    :editor
-    @update="editor.focus()"
-  />
+  <div class="container">
+    <button @click="showImagePanel = !showImagePanel">
+      <c-icon class="icon" name="pencil" />
+    </button>
+
+    <button @click="editor.removeBlocks([image.id])">
+      <c-icon class="icon" name="trash" />
+    </button>
+  </div>
+
+  <div v-if="showImagePanel" class="imageSelector">
+    <ImageFilePanel
+      :current-block="image"
+      :link-edition-ctx
+      :editor
+      @update="editor.focus()"
+    />
+  </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  background-color: white;
+  box-shadow: 0 1px 1px 1px #0f0f0f;
+  border-radius: 5px;
+}
+
+button {
+  display: block;
+  border-radius: 4px;
+  height: 30px;
+  max-height: 30px;
+  width: 30px;
+}
+
+button:hover {
+  background-color: #efefef;
+}
+
+.icon {
+  font-size: 1.1rem;
+  color: #3f3f3f;
+  height: 30px;
+}
+
+/*
+  NOTE: Popover is implemented manually here due to an unresolved bug in the library we'd like to use
+  Once that issue is resolved, this code block will be removed and the library will be used instead
+  Consider this a temporary "dirty" hack
+*/
+.imageSelector {
+  position: absolute;
+  left: 0;
+  /* Yes, this is dirty */
+  top: 2.5rem;
+  background: white;
+  box-shadow: 0px 4px 12px #cfcfcf;
+  border-radius: 6px;
+}
+</style>
