913622cf3aa17c3aa3da2240838c0e291b132731
Clément E. <clement.eraud@xwiki.com>
2025-06-23 08:58:24 +0200

    update

--- editors/blocknote-react/src/blocknote/dictionary/index.ts
+++ editors/blocknote-react/src/blocknote/dictionary/index.ts
@@ -1,5 +1,11 @@
 import { enDictionary } from "./en";
 
+/**
+ * The selected dictionary is to be embedded inside of the BlockNote editor instance through
+ * the `createDictionary` function.
+ *
+ * It will then be usable in any component inside BlockNote using the `useCustomDictionary` hook
+ */
 export const customDictionaries = {
   en: enDictionary,
 };
