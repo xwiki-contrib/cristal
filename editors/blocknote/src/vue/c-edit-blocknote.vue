5e323a46946a871d49a1cab48101f9badd6a34a4
Clément E. <clement.eraud@xwiki.com>
2026-03-24 09:16:41 +0100

    update

--- editors/blocknote/src/vue/c-edit-blocknote.vue
+++ editors/blocknote/src/vue/c-edit-blocknote.vue
@@ -83,7 +83,8 @@ const modelReferenceHandler = container
 const alertsService = container.get<AlertsService>("AlertsService")!;
 const storage = container.get<StorageProvider>("StorageProvider").get();
 
-const { realtimeURL: realtimeServerURL } = cristal.getWikiConfig();
+const { realtimeURL: realtimeServerURL, syntaxes } = cristal.getWikiConfig();
+
 let collaborationProvider: () => CollaborationInitializer;
 let status: Ref<Status> | undefined;
 let users: Ref<User[]> | undefined;
@@ -137,6 +138,7 @@ const contextForMacros: ContextForMacros = {
  *
  * @param currentPage - The fetched current page
  */
+// eslint-disable-next-line max-statements
 async function loadEditor(currentPage: PageData | undefined): Promise<void> {
   if (!currentPage) {
     // TODO
@@ -149,11 +151,22 @@ async function loadEditor(currentPage: PageData | undefined): Promise<void> {
     return;
   }
 
+  const syntaxConfig = syntaxes.find(
+    (syntax) => syntax.id === currentPage.syntax,
+  );
+
+  if (!syntaxConfig) {
+    // TODO add a translation
+    unknownSyntax.value = `Syntax [${currentPage.syntax}] is not supported by the current backend`;
+    return;
+  }
+
   editorProps.value = {
     theme: "light",
     // TODO: make this customizable
     // https://jira.xwiki.org/browse/CRISTAL-457
     lang: "en",
+    syntax: syntaxConfig,
   };
 
   editorContent.value = await markdownToUniAst.parseMarkdown(
